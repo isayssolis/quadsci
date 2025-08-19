909cb107f8c2ed53a244988c8344ff0958be5f3d
Isays Solis <isaysmail@gmail.com>
2025-08-18 22:34:26 -0600

    add css
    add css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header.vue'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.component('Header', Header);
 
 app.mount('#app')
